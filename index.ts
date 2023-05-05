3fc4b3bfa813207995d05868e643b121da3e125f
Vivy Vuong <vngvy48@gmail.com>
2023-05-05 17:39:00 -0400

    Add cors

--- index.ts
+++ index.ts
@@ -2,13 +2,18 @@ import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import surveyRoute from "./routes/api/survey";
 import reportRoute from "./routes/api/report";
+import cors from 'cors';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.json())
+
+app.use(express.json());
+app.use(cors({
+    origin: "*"
+}));
 app.use("/api/survey", surveyRoute);
 app.use("/api/report", reportRoute);
 
