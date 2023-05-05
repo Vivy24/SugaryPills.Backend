50def8714c99ac3f45d51609244a5ee51755596a
Vivy Vuong <vngvy48@gmail.com>
2023-05-05 12:51:59 -0400

    Updated index.ts

--- index.ts
+++ index.ts
@@ -12,10 +12,9 @@ app.use(express.json())
 app.use("/api/survey", surveyRoute);
 app.use("/api/report", reportRoute);
 
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
 
-// app.listen(port, () => {
-//     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-// });
-
-app.listen({ port: port, host: "0.0.0.0" });
+// app.listen({ port: port, host: "0.0.0.0" });
 
