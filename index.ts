8cdd08d906e068873fd3f2533a1cd8b5e2721462
Vivy Vuong <vngvy48@gmail.com>
2023-05-05 02:55:14 -0400

    Deploy to Fly.io

--- index.ts
+++ index.ts
@@ -6,13 +6,16 @@ import reportRoute from "./routes/api/report";
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 
 app.use(express.json())
 app.use("/api/survey", surveyRoute);
 app.use("/api/report", reportRoute);
 
 
-app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+// });
+
+app.listen({ port: port, host: "0.0.0.0" });
+
