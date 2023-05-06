3ae5444e8d30a1278cafb811fa4bb07c3dce2c8d
Vivy Vuong <vngvy48@gmail.com>
2023-05-05 22:21:06 -0400

    Mapping report API input

--- helpers/map.ts
+++ helpers/map.ts
@@ -65,10 +65,6 @@ export const mapLifeStyleResult = (inputLifeStyle: any) => {
         question5Str += 'other reasons,'
     }
 
-    if (question5Str == "") {
-        question5Str = "happy,"
-    }
-
 
     const formattedLifestyle: LifeStyleResult = {
         question1: question1Str.slice(0, question1Str.length - 1),
@@ -79,4 +75,73 @@ export const mapLifeStyleResult = (inputLifeStyle: any) => {
     }
 
     return formattedLifestyle;
+}
+
+export const mapReportLifestyle = (inputReportLifeStyle: any) => {
+    let question2Str = "";
+    switch (inputReportLifeStyle.question2) {
+        case 'once':
+            question2Str = 'once a week';
+            break;
+        case 'sometimes':
+            question2Str = '2-5 times a week';
+            break;
+        case 'everyday':
+            question2Str = 'once a month';
+            break;
+    }
+
+
+    let question3Str = "";
+    switch (inputReportLifeStyle.question3) {
+        case 'month':
+            question3Str = 'once a month';
+            break;
+        case 'quarter':
+            question3Str = 'once a quarter';
+            break;
+        case 'year':
+            question3Str = 'once a year';
+            break;
+    }
+
+    let question4Str = "";
+    if (inputReportLifeStyle.question4.includes("airPollution")) {
+        question4Str += 'air pollution,'
+    }
+    if (inputReportLifeStyle.question4.includes("waterPollution")) {
+        question4Str += 'lack of clean drinking water,'
+    }
+    if (inputReportLifeStyle.question4.includes("toxins")) {
+        question4Str += 'exposure to toxins,'
+    }
+
+
+
+    let question5Str = "";
+    if (inputReportLifeStyle.question5.includes("trauma")) {
+        question5Str += 'historical trauma,'
+    }
+    if (inputReportLifeStyle.question5.includes("finances")) {
+        question5Str += 'financial issue,'
+    }
+    if (inputReportLifeStyle.question5.includes("other")) {
+        question5Str += 'other reasons,'
+    }
+
+
+
+
+    let result = {
+        lifestyles: {
+            question1: inputReportLifeStyle.question1.join(","),
+            question2: question2Str,
+            question3: question3Str,
+            question4: question4Str.slice(0, question4Str.length - 1),
+            question5: question5Str.slice(0, question5Str.length - 1)
+        }
+    }
+
+    console.log(result);
+    return result;
 }
--- routes/api/report.ts
+++ routes/api/report.ts
@@ -2,6 +2,8 @@ import express, { Express, Request, Response } from 'express';
 import { LifeStyleResult } from '../../models/type';
 import { poolQuery } from '../../database/queries';
 import { calculateReport } from '../../helpers/report';
+import { mapReportLifestyle } from '../../helpers/map';
+
 const router = express.Router();
 
 // @route GET /api/report
@@ -9,9 +11,9 @@ const router = express.Router();
 // @access public
 router.post("/", async (req: Request, res: Response) => {
     try {
-        const filterRequest: LifeStyleResult = req.body.lifestyles;
-        const filterIDs = await poolQuery.getArrayOfIDFromLifestyle(filterRequest);
-        let result;
+        const filterRequest = await mapReportLifestyle(req.body);
+        const filterIDs = await poolQuery.getArrayOfIDFromLifestyle(filterRequest.lifestyles as LifeStyleResult);
+        let result = {};
         if (filterIDs && filterIDs.length > 0) {
             result = await calculateReport(filterIDs);
         }
